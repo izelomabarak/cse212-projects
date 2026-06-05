452af78be3fbee0c748dafa40c396993b7a2d3e9
izelomabarak <joseluisim.lds@gmail.com>
2026-06-04 22:10:27 -0600

    Week06

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -12,34 +12,77 @@ public class Node
     public void Insert(int value)
     {
         // TODO Start Problem 1
+        if (value != Data && value != Left?.Data && value != Right?.Data)
+        {
+            if (value < Data)
+            {
+                // Insert to the left
+                if (Left is null)
+                    Left = new Node(value);
+                else
+                    Left.Insert(value);
 
-        if (value < Data)
-        {
-            // Insert to the left
-            if (Left is null)
-                Left = new Node(value);
+            }
             else
-                Left.Insert(value);
-        }
-        else
-        {
-            // Insert to the right
-            if (Right is null)
-                Right = new Node(value);
-            else
-                Right.Insert(value);
+            {
+                // Insert to the right
+                if (Right is null)
+                    Right = new Node(value);
+                else
+                    Right.Insert(value);
+            }
         }
     }
 
     public bool Contains(int value)
     {
-        // TODO Start Problem 2
-        return false;
+        if (value == Data)
+        {
+            return true;
+        }
+        if (value < Data)
+        {
+            if (Left is not null)
+            return Left.Contains(value);
+        }
+        else
+        {
+            if (Right is not null)
+            return Right.Contains(value);
+        }
+        return false; 
     }
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int height = 1;
+        int heightLeft = 0;
+        int heighRigth = 0;
+        if (Left is not null)
+        {
+            heightLeft =  Left.GetHeight();
+        }
+        if (Right is not null)
+        {
+            heighRigth = Right.GetHeight();
+        }
+        if (Left is null && Right is null)
+        {
+            return 1;
+        }
+        if (heightLeft == heighRigth)
+        {
+            return height + heightLeft;
+        }
+        if (heightLeft > heighRigth)
+        {
+            return height + heightLeft;
+        }
+        if (heightLeft < heighRigth)
+        {
+            return height + heighRigth;
+        }
+        return height;
     }
 }
